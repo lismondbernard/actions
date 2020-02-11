57b5914455497accb84b33e5c7cdcd410128bfff
Tyler Suggs <tyler@acst.com>
2020-02-11 17:12:10 -0500

    testing api call return values for redirects

--- github-release-download/src/github.ts
+++ github-release-download/src/github.ts
@@ -14,8 +14,8 @@ class AcstGitHub {
   }
 
   async downloadFromUrl(url: string): Promise<httpm.HttpClientResponse> {
-    const bh = new hm.PersonalAccessTokenCredentialHandler(this.token);
-    const http = new httpm.HttpClient("acst/github-release-download", [bh], {
+    // const bh = new hm.PersonalAccessTokenCredentialHandler(this.token);
+    const http = new httpm.HttpClient("acst/github-release-download", [], {
       allowRedirects: true,
       allowRetries: true,
       maxRetries: 3
